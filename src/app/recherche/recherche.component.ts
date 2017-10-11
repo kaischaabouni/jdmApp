13f5aed4dd39f7fc504b9020d22f0c1661f5d37c
Kaïs Chaâbouni <kais9chaabouni@gmail.com>
2017-10-11 21:26:58 +0200

    améliorer affichage résultat

--- src/app/recherche/recherche.component.ts
+++ src/app/recherche/recherche.component.ts
@@ -12,8 +12,8 @@ import { Relation } from '../modeles/relation';
 export class RechercheComponent implements OnInit {
 
     definitions: String[];
-    rt0: Relation;
-    rt1: Relation;
+    rt0 = {sortantes : "", entrantes: ""};
+    rt1 = {sortantes : "", entrantes: ""};
 
     constructor(private _rechercheService: RechercheService) { }
 
@@ -25,8 +25,22 @@ export class RechercheComponent implements OnInit {
             this._rechercheService.getResultatRecherche(termeRecherche.value)
                 .subscribe(resRechercheData => {
                     this.definitions = resRechercheData.definitions;
-                    this.rt0 = resRechercheData.relations.rt0;
-                    this.rt1 = resRechercheData.relations.rt1;
+
+                    // rt0
+                    for(let association of resRechercheData.relations.rt0.sortantes){
+                        this.rt0.sortantes = this.rt0.sortantes + association.nom + "; ";
+                    }
+                    for(let association of resRechercheData.relations.rt0.entrantes){
+                        this.rt0.entrantes = this.rt0.entrantes + association.nom + "; ";
+                    }
+
+                    // rt1
+                    for(let association of resRechercheData.relations.rt1.sortantes){
+                        this.rt1.sortantes = this.rt0.sortantes + association.nom + "; ";
+                    }
+                    for(let association of resRechercheData.relations.rt1.entrantes){
+                        this.rt1.entrantes = this.rt0.entrantes + association.nom + "; ";
+                    }
                 });
         };
     }
