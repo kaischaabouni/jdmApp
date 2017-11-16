a2bd081094e2bfff5448ea7b42fd5dabecddbffa
Kaïs Chaâbouni <kais9chaabouni@gmail.com>
2017-11-16 20:21:59 +0100

    port 3000

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('*',(req,res)=> {
   res.sendFile(path.join(__dirname,'dist/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+//app.listen(process.env.PORT || 8080);
 
-//app.listen(3000, function(){
-//    console.log("listen to port 3000");
-//  }); 
+app.listen(3000, function(){
+    console.log("listen to port 3000");
+}); 
